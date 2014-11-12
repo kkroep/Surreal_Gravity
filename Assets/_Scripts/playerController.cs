036d6e64c294ccfc92c6f3f8d5e5f79261fb9f44
kkroep <k.kroep1@gmail.com>
2014-11-12 16:23:01 +0100

    working fps movearound without left-right

--- Assets/_Scripts/playerController.cs
+++ Assets/_Scripts/playerController.cs
@@ -1,26 +1,59 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿	using UnityEngine;
+	using System.Collections;
 
-public class playerController : MonoBehaviour {
-	public Vector3 Start_Gravity = new Vector3(1f, 0f, 0f);
-
-	// Use this for initialization
-	void Start () {
+	public class playerController : MonoBehaviour {
+	#region [init look around]
+	public float speed = 3.0F;
+	public float jumpSpeed = 8.0F; 
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
 	
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 };
+	public RotationAxes axes = RotationAxes.MouseX;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	
+	float rotationY = 0F;
+	#endregion
+
+	void Start()
+	{
+		rigidbody.freezeRotation = true;
 	}
-	
-	void FixedUpdate () {
-		if(networkView.isMine || offline){
-			//This is the freeze option controlled by gameController
-			//if(gameController.Freeze_Counter==0f){
 
-			#region [velocity]
-			//Movement in direction of the object
-			Vector3 AddPos = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0f);
-			rigidbody.velocity = AddPos * speed*Time.fixedDeltaTime;
-			//rigidbody.AddForce(current_normal*-Gravity_Strength);
-			#endregion
-			//}
+	void Update ()
+	{
+		#region [look around]
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+			
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
 		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+		}
+		#endregion
+
+		rigidbody.velocity = transform.forward * speed * Input.GetAxis("Vertical");
+
 	}
+
 }
