61a65d994c1c9b7c56f1085548676bfd53a6421d
kkroep <k.kroep1@gmail.com>
2014-11-12 14:21:07 +0100

    progress

--- Assets/_Scripts/playerController.cs
+++ Assets/_Scripts/playerController.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class playerController : MonoBehaviour {
+	public Vector3 Start_Gravity = new Vector3(1f, 0f, 0f);
 
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		if(networkView.isMine || offline){
+			//This is the freeze option controlled by gameController
+			//if(gameController.Freeze_Counter==0f){
+
+			#region [velocity]
+			//Movement in direction of the object
+			Vector3 AddPos = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0f);
+			rigidbody.velocity = AddPos * speed*Time.fixedDeltaTime;
+			//rigidbody.AddForce(current_normal*-Gravity_Strength);
+			#endregion
+			//}
+		}
 	}
 }
