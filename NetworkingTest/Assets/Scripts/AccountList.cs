30c33a69eaa68c7230ce0f617b6404ff5c3ed1d5
Earless <roberto_mol@hotmail.com>
2014-11-18 08:55:05 +0100

    Networking stuff + semi accounts
    Je kan 'accounts' maken en je kan soort van inloggen.
    TODO: Code cleanen
    TODO: Class proberen
    TODO: Accounts.txt op een WebServer zetten

---
+++ NetworkingTest/Assets/Scripts/AccountList.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+
+public class AccountList : MonoBehaviour {
+
+	private List<Account> Acclist;
+    //string pathoffile = @"C:\Users\Roby\Documents\TU Delft\Minor\UnityProjects\NetworkingTest\Accounts.txt";
+
+	public AccountList ()
+	{
+		Acclist = new List<Account> ();
+	}
+
+	public int sizeList
+	{
+		get { return Acclist.Count; }
+	}
+
+	public Account indexOf (int i)
+	{
+		return Acclist[i];
+	}
+
+	public void addAccount (Account acc)
+	{
+		if (!Acclist.Contains(acc))
+		{
+			Acclist.Add(acc);
+		}
+	}
+
+	public static AccountList readAccounts ()
+	{
+		FileStream stream = new FileStream (@"C:\Users\Roby\Documents\TU Delft\Minor\UnityProjects\NetworkingTest\Accounts.txt", FileMode.Open);
+		BinaryReader bread = new BinaryReader (stream);
+		List<Account> acclijst = new List<Account> ();
+		AccountList al = new AccountList ();
+		int i = 0;
+		while(i < 3)
+		{
+			string name = bread.ReadString();
+			string password = bread.ReadString();
+			Debug.Log("Name: " + name + ", Pass: " + password);
+			Account acc = new Account (name, password);
+			acclijst.Add(acc);
+			al.addAccount(acclijst[i]);
+			i++;
+		}
+		bread.Close();
+		return al;
+	}
+
+}
---
+++ NetworkingTest/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour{
+
+	public int speed;
+	public int gravity;
+
+	private CharacterController charcontr;
+
+	// Use this for initialization
+	void Start () {
+
+		charcontr = GetComponent<CharacterController>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (networkView.isMine)
+		{
+			Vector3 input = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, -gravity * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+			charcontr.Move(input);
+		}
+		else
+		{
+			enabled = false;
+		}
+	
+	}
+}
