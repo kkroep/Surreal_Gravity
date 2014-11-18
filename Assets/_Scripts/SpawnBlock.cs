9e5db39000b2f8dd2c5bbed090b5b045513d1690
StevenH <stevenhelm@live.nl>
2014-11-18 12:44:58 +0100

    update procedural level generation

--- Assets/_Scripts/SpawnBlock.cs
+++ Assets/_Scripts/SpawnBlock.cs
@@ -36,59 +36,68 @@ public class SpawnBlock : MonoBehaviour {
 		Vector3 add;
 		int checkResult;
 
+		List<int> xList = new List<int>();
+		xList.Add (-1);xList.Add (1);
+		List<int> yList = new List<int>();
+		yList.Add (-1);yList.Add (1);
+		List<int> zList = new List<int>();
+		zList.Add (-1);zList.Add (1);
+
+		float positionchange1 = Mathf.CeilToInt (Random.value*100);
+		int positionchange2 = 0;
+		float xInterval = (100/3);
+		float yInterval = (100/3);
+		float zInterval = (100/3);
+		
+		
+		if(settings.xScaling > 1 && xList.Count>0){
+			xInterval = ((100/3)*settings.xScaling);
+			yInterval = (100-xInterval)/2;
+			zInterval = yInterval;
+		}
+		else if(settings.yScaling > 1 && yList.Count>0){
+			yInterval = ((100/3)*settings.yScaling);
+			xInterval = (100-yInterval)/2;
+			zInterval = xInterval;
+		}
+		else{
+			zInterval = ((100/3)*settings.zScaling);
+			xInterval = (100-zInterval)/2;
+			yInterval = xInterval;
+		}
+		
 		do{
 			if(settings.maxOneDirectionOfChange){
-				List<int> xList = new List<int>();
-				xList.Add (-1);xList.Add (1);
-				List<int> yList = new List<int>();
-				yList.Add (-1);yList.Add (1);
-				List<int> zList = new List<int>();
-				zList.Add (-1);zList.Add (1);
-
-				float positionchange1 = Mathf.CeilToInt (Random.value*100);
-				int positionchange2;
-				float xInterval = (100/3);
-				float yInterval = (100/3);
-				float zInterval = (100/3);
-
-
-				if(settings.xScaling > 1 && xList.FindLastIndex>0){
-					xInterval = ((100/3)*settings.xScaling);
-					yInterval = (100-xInterval)/2;
-					zInterval = yInterval;
-				}
-				else if(settings.yScaling > 1 && yList.FindLastIndex>0){
-					yInterval = ((100/3)*settings.yScaling);
-					xInterval = (100-yInterval)/2;
-					zInterval = xInterval;
-				}
-				else{
-					zInterval = ((100/3)*settings.zScaling);
-					xInterval = (100-zInterval)/2;
-					yInterval = xInterval;
-				}
 
 
 
-				if(positionchange1<xInterval){
+				int temprandom = 0;
+				int direction = 0;
+
+
+				if(yList.Count == 0|| xList.Count == 0 || zList.Count == 0)
+					Debug.Log ("klopt niet");
+
+				if(positionchange1<xInterval && xList.Count>0){
+					temprandom = Mathf.CeilToInt(Random.value*xList.Count);
 					positionchange2 = 1;
+					direction = xList[temprandom-1];
+					xList.RemoveAt(temprandom-1);
 				}
-				else if(positionchange1<(xInterval + yInterval)){
+				else if(positionchange1<(xInterval + yInterval) && yList.Count>0){
+					temprandom = Mathf.CeilToInt(Random.value*yList.Count);
 					positionchange2 = 2;
+					direction = yList[temprandom-1];
+					yList.RemoveAt(temprandom-1);
 				}
-				else{
+				else if(positionchange1<(xInterval + yInterval + zInterval) && zList.Count>0){
+					temprandom = Mathf.CeilToInt(Random.value*zList.Count);
 					positionchange2 = 3;
+					direction = zList[temprandom-1];
+					zList.RemoveAt(temprandom-1);
 				}
 
-				int direction;
 
-				float temprandom = Random.value;
-				if(Random.value<0.5){
-					direction = -1;
-				}
-				else{
-					direction = 1;
-				}
 
 				add = new Vector3(0,0,0);
 
@@ -97,8 +106,9 @@ public class SpawnBlock : MonoBehaviour {
 					add.x = direction;
 				else if(positionchange2 == 2)
 					add.y = direction;
-				else(positionchange2 == 3)
+				else if(positionchange2 == 3)
 					add.z = direction;
+
 
 
 
@@ -106,43 +116,14 @@ public class SpawnBlock : MonoBehaviour {
 			
 			else{
 				add = new Vector3(Mathf.RoundToInt((Random.value*2) - 1),Mathf.RoundToInt((Random.value*2) - 1),Mathf.RoundToInt((Random.value*2) - 1));
-				/*
-				int i = 20;
-				while((Mathf.Abs (add.x) + Mathf.Abs (add.y) + Mathf.Abs (add.z))>1 && i >0){
-					int zerosetter = Mathf.CeilToInt (Random.value*3);
-
-					Debug.Log (zerosetter);
-
-					if(zerosetter == 1)
-						add.x = 0;
-					else if(zerosetter == 2)
-						add.y = 0;
-					else if(zerosetter == 3)
-						add.z = 0;
-
-					i--;
-
-				}
-
-				if(!(add.x > 0)){
-					add.y += Mathf.RoundToInt((Random.value*2) - 1);
-				}
-				if(!(add.x > 0 && add.y>0)){
-					add.z += Mathf.RoundToInt((Random.value*2) - 1);
-
-				}
-				*/
 			}
 
 			targetPosition = this.transform.position + add;
 			checkResult = Physics.OverlapSphere( targetPosition, checkRadius ).Length;
-			iterations++;
-
-
-			
+			iterations++;			
 			
 		}while (checkResult>0 && iterations<50);
-		Debug.Log (iterations);
+
 
 		GameObject go = (GameObject)Instantiate (buildingBlock, targetPosition, Quaternion.identity);
 		go.SendMessage ("startUp", spawn);
