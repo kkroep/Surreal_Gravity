ea3012de978eb1f90ea795c705be4285faa8f81d
StevenH <stevenhelm@live.nl>
2014-11-25 12:52:07 +0100

    started working on robot

---
+++ Assets/MainRobotSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainRobotSettings : MonoBehaviour {
+	
+	public int numberRobots;
+
+	private int selectBlock;
+
+	void Start(){
+		selectBlock = numberRobots;
+	}
+	
+	
+}
---
+++ Assets/RobotScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotScript : MonoBehaviour {
+
+	private bool needsSelection;
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
