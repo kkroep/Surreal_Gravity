06594a5dd75099aa5af709576e3decee931bb5a9
kkroep <k.kroep1@gmail.com>
2014-11-26 16:15:51 +0100

    fancy menu

---
+++ Assets/_Scripts/MenuButtons.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour {
+
+	public Color Button_Idle = Color.green;
+	public Color Button_Active = Color.red;
+
+	// Use this for initialization
+	void OnMouseEnter(){
+		renderer.material.color = Color.white;
+	}
+
+	void Start(){
+		Debug.Log("test");
+		renderer.material.color = Button_Idle;
+	}
+
+	void OnMouseExit(){
+		Debug.Log("test");
+		renderer.material.color = Button_Idle;
+	}
+
+}
