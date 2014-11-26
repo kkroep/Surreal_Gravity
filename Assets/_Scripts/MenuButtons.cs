dfb23d75203ca3fea661b6905dcbbb0c19fa66eb
kkroep <k.kroep1@gmail.com>
2014-11-26 17:09:38 +0100

    menu functionality improved

--- Assets/_Scripts/MenuButtons.cs
+++ Assets/_Scripts/MenuButtons.cs
@@ -5,17 +5,23 @@ public class MenuButtons : MonoBehaviour {
 	public Color Button_Idle = Color.green;
 	public Color Button_Active = Color.red;
 
+	public bool HoverEffects = true;
 	public bool MultiPlayer = false;
 	public bool Back = false;
 	public bool QuitGame = false;
 	public bool StartServer = false;
 	public bool Refresh = false;
+	public bool Disconnect = false;
+
 
 	public GameObject Main_Menu;
 	public GameObject Multiplayer_Menu;
+	public GameObject Server_Menu;
 
+	
 	// Use this for initialization
 	void OnMouseEnter(){
+		if(HoverEffects)
 		renderer.material.color = Color.white;
 	}
 
@@ -24,20 +30,22 @@ public class MenuButtons : MonoBehaviour {
 	}
 
 	void OnMouseExit(){
+		if(HoverEffects)
 		renderer.material.color = Button_Idle;
 	}
 
 	void OnMouseUp(){
-		renderer.material.color = Button_Idle;
 
 		if (MultiPlayer) {
 			Main_Menu.SetActive(false);
 			Multiplayer_Menu.SetActive(true);
+			renderer.material.color = Button_Idle;
 		}
 		
 		if (Back) {
 			Main_Menu.SetActive(true);
 			Multiplayer_Menu.SetActive(false);
+			renderer.material.color = Button_Idle;
 		}
 
 		if (QuitGame) {
@@ -45,11 +53,19 @@ public class MenuButtons : MonoBehaviour {
 		}
 
 		if (StartServer) {
-			Debug.Log ("Started The server");
+			Server_Menu.SetActive(true);
+			Multiplayer_Menu.SetActive(false);
+			renderer.material.color = Button_Idle;
 		}
 
 		if (Refresh) {
 			Debug.Log ("Refreshed");
+		}
+
+		if (Disconnect) {
+			Server_Menu.SetActive(false);
+			Multiplayer_Menu.SetActive(true);
+			renderer.material.color = Button_Idle;
 		}
 	}
 
