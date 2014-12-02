960970bfddaa505b1771ba751f519286a6ec3d49
StevenH <stevenhelm@live.nl>
2014-12-02 16:40:13 +0100

    Started working on path finder

---
+++ Assets/_Scripts/Node.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node{
+
+
+
+	public int heuristicValue = 0;
+	public int movementCost = 0;
+	public int totalCost = 0;
+	public Node parentNode = null;
+	public Node widthneg = null;
+	public Node widthpos = null;
+	public Node heightneg = null;
+	public Node heightpos = null;
+	public Node depthneg = null;
+	public Node depthpos = null;
+
+
+	public int xPosition;
+	public int yPosition;
+	public int zPosition;
+
+	public bool canPass;
+
+	public Node(int x, int y, int z){
+		xPosition = x;
+		yPosition = y;
+		zPosition = z;
+
+	}
+
+
+}
---
+++ Assets/_Scripts/Pathfinder.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pathfinder : MonoBehaviour{
+
+	private List<Node> openList = new List<Node>();
+	private List<Node> closedList = new List<Node>();
+	private Node checkingNode = null;
+	public Node startNode = null;
+	public Node targetNode = null;
+	public bool foundTarget = false;
+	public int baseMovementCost = 10;
+	public LevelCreator level;
+
+	private GameObject levelcreator;
+	private int[,,] grid;
+	private Node[,,] nodeGrid;
+
+	void Start(){
+		levelcreator = GameObject.FindGameObjectWithTag("levelSettings");
+		level = levelcreator.GetComponent<LevelCreator>();
+		grid = level.getGrid ();
+		nodeGrid = new Node[level.levelWidth,level.levelHeight,level.levelDepth];
+
+		for(int width=0;width<30;width++){
+			for (int height=0;height<30;height++){
+				for (int depth=0;depth<30;depth++){
+					Debug.Log (depth);
+					Node temp =  new Node(width,height,depth);
+					temp.heuristicValue = CalculateHeuristicValue(temp);
+					if(width>0){
+						temp.widthneg = nodeGrid[width-1,height,depth];
+					}
+					if(width<level.levelWidth-1){
+						temp.widthpos = nodeGrid[width+1,height,depth];
+					}
+					if(height>0){
+						temp.heightneg = nodeGrid[width,height-1,depth];
+					}
+					if(height<level.levelHeight-1){
+						temp.heightpos = nodeGrid[width,height+1,depth];
+					}
+					if(depth>0){
+						temp.depthneg = nodeGrid[width,height,depth-1];
+					}
+					if(depth<level.levelDepth-1){
+						temp.depthpos = nodeGrid[width,height,depth+1];
+					}
+
+					nodeGrid[width,height,depth] = temp;
+				}
+			}
+		}
+
+		checkingNode = nodeGrid[1,1,1];
+
+		//CalculateAllHeuristics();
+		startNode = checkingNode;
+
+
+	}
+
+	void Update(){
+		if (foundTarget == false){
+			FindPath();
+		}
+
+		if (foundTarget == true){
+			TraceBackPath ();
+		}
+
+
+	}
+
+
+	public int CalculateHeuristicValue(Node temp){
+		int heuristic = Mathf.Abs (targetNode.xPosition - temp.xPosition) + Mathf.Abs (targetNode.yPosition - temp.yPosition) + Mathf.Abs (targetNode.zPosition - temp.zPosition);
+		return heuristic;
+	}
+
+	private void FindPath(){
+		if (foundTarget == false){
+			if (checkingNode.widthneg != null)
+				DetermineNodeValues(checkingNode,checkingNode.widthneg);
+			if (checkingNode.widthpos != null)
+				DetermineNodeValues(checkingNode,checkingNode.widthpos);
+			if (checkingNode.heightneg != null)
+				DetermineNodeValues(checkingNode,checkingNode.heightneg);
+			if (checkingNode.heightpos != null)
+				DetermineNodeValues(checkingNode,checkingNode.heightpos);
+			if (checkingNode.depthneg != null)
+				DetermineNodeValues(checkingNode,checkingNode.depthneg);
+			if (checkingNode.depthpos != null)
+				DetermineNodeValues(checkingNode,checkingNode.depthpos);
+
+		}
+
+
+	}
+
+	private void TraceBackPath(){
+
+
+	}
+
+	private void DetermineNodeValues(Node checkingNode, Node other){
+
+	}
+
+
+
+
+
+}
