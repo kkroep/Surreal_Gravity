41b3891f5d793dc794f029d3efef484fd7d19723
StevenH <stevenhelm@live.nl>
2014-12-04 12:18:25 +0100

    blip

--- Assets/RobotMovement.cs
+++ Assets/RobotMovement.cs
@@ -3,22 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class RobotMovement : MonoBehaviour {
-	public Pathfinder pathfind;
+	private Pathfinder pathfind;
 
 	private List<Node> path;
-
+/*
 	// Update is called once per frame
 	void Update () {
 		pathfind = this.GetComponent<Pathfinder>();
 		if(pathfind.tracedBack == true){
 			path = pathfind.path;
-			for (int i=0;i<path.Count;i++){
-
-			}
+			MoveRobot (new Vector3(2,2,2),new Vector3(2,5,2),3.0);
 		}
 	
 	}
-	/*
+
 	function MoveObject (startPos : Vector3, endPos : Vector3, moveTime : float)
 	{
 		var i = 0.0;
@@ -31,15 +29,11 @@ public class RobotMovement : MonoBehaviour {
 		}
 	}
 
-	public MoveObject(Vector3 startPos, Vector3 endPos, float moveTime){
-		float i = 0;
-		float rate = 1.0/moveTime;
-		while(i<1.0){
-			i += Time.deltaTime*rate;
-			transform.position = Vector3.Lerp (startPos,endPos, Mathf.SmoothStep (0.0,1.0,i));
-			                               
 
-		}
+	public void MoveRobot(Vector3 startPos, Vector3 endPos, float moveTime){
+		transform.position = Vector3.Lerp (startPos,endPos, Time.deltaTime);			                               
+
 	}
-	*/
+*/
+
 }
