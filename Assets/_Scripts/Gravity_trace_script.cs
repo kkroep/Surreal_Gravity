7d5250c17d96b396e4e5ea3848aa25acb3c58b7c
kkroep <k.kroep1@gmail.com>
2014-12-10 15:24:25 +0100

    laser added :D

---
+++ Assets/_Scripts/Gravity_trace_script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity_trace_script : MonoBehaviour {
+	public float Time2Live = 0.5f;
+	private float Time2Live_left;
+
+	void Start(){
+		Time2Live_left = Time2Live;
+	}
+
+	void FixedUpdate (){
+		Time2Live_left -= Time.fixedDeltaTime;
+		//Debug.Log (Time2Live_left);
+		if(Time2Live_left<0)
+			Destroy (gameObject);
+	}
+}
