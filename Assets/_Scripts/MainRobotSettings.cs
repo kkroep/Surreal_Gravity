75d54b1f6e573642d6c0ba0515dc7d44aae5c196
Earless <roberto_mol@hotmail.com>
2014-12-17 16:56:20 +0100

    trying to fix single player

--- Assets/_Scripts/MainRobotSettings.cs
+++ Assets/_Scripts/MainRobotSettings.cs
@@ -12,7 +12,7 @@ public class MainRobotSettings : MonoBehaviour {
 
 
 	void Start(){
-		if(Network.isServer){
+		if(Network.isServer && !BasicFunctions.playOffline){
 			for(int i=0;i<numberRobots;i++){
 				Network.Instantiate (robot, new Vector3(10,10,10), Quaternion.identity,0);
 			}
