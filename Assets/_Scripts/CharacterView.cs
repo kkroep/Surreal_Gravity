20dcbe9e0c70da5d59979fa27699c3b51b2b9aa8
Kwok Hou <k.h.man-1@student.tudelft.nl>
2014-12-17 14:25:05 +0100

    character textures

--- Assets/_Scripts/CharacterView.cs
+++ Assets/_Scripts/CharacterView.cs
@@ -8,29 +8,27 @@ public class CharacterView : MonoBehaviour {
 	private GameObject armMesh;
 	private GameObject armsArmature;
 
-	// Use this for initialization
 	void Start () {
 		armature = GameObject.Find ("Armature");
 		circle = GameObject.Find ("Circle");
-		armMesh = GameObject.Find ("ArmMesh");
-		armsArmature = GameObject.Find ("ArmsArmature");
+		/*armMesh = GameObject.Find ("ArmMesh");
+		armsArmature = GameObject.Find ("ArmsArmature");*/
 
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		if(networkView.isMine)
 		{
 			Destroy(armature);
 			Destroy(circle);
-			
-			// or u can use childB.renderer.enabled=false; if u just want to hide it
+
 		}
+		/*
 		else
 		{
 			Destroy(armMesh);
 			Destroy(armsArmature);
 			
-		}
+		}*/
 	}
 }
