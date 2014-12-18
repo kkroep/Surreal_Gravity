68f637c535fd8e79d1e697665ffda5c1ab8c32cb
coenbers <C.W.J.Berssenbrugge@student.tudelft.nl>
2014-12-18 14:23:34 +0100

    Random material select should work on client now
    Material selected with materialscript on prefab of block

---
+++ Assets/_Scripts/MaterialSelect.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialSelect : MonoBehaviour {
+	public Material[] MaterialList;
+	
+	// Randomly select a material from the list when instantiated
+	void Start () {
+		renderer.material = MaterialList [(int) (Random.Range (0, MaterialList.Length-0.0001f))];
+	}
+}
