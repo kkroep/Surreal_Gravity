57240f66c8da95baa86c32da931ed343e648457d
Earless <roberto_mol@hotmail.com>
2015-01-13 11:18:51 +0100

    Playercontroller fix

--- Assets/_Scripts/playerController.cs
+++ Assets/_Scripts/playerController.cs
@@ -180,7 +180,6 @@ public class playerController : MonoBehaviour
 
 		void Update ()
 		{
-<<<<<<< HEAD
 			if (Input.GetKeyDown (KeyCode.Escape)) {
 				if (!spawnScript) {
 						spawnScript = GameObject.FindGameObjectWithTag ("SpawnTag").GetComponent<NW_Spawning> ();
@@ -193,37 +192,36 @@ public class playerController : MonoBehaviour
 						spawnScript.closeClientInGame ();
 				} else if (BasicFunctions.playOffline) {
 						Application.LoadLevel ("Menu_New");
-=======
+				}
 				if (Input.GetKeyDown (KeyCode.Escape)) {
-						if (!spawnScript) {
-								spawnScript = GameObject.FindGameObjectWithTag ("SpawnTag").GetComponent<NW_Spawning> ();
-						}
-						string gamemode;
+					if (!spawnScript) {
+							spawnScript = GameObject.FindGameObjectWithTag ("SpawnTag").GetComponent<NW_Spawning> ();
+					}
+					string gamemode;
 
-						if (Network.isServer) {
-								dontDestroy = true;
+					if (Network.isServer) {
+							dontDestroy = true;
 
-								if(BasicFunctions.ForkModus){
-									gamemode = "FORK";
-								}
-								else{
-									gamemode = "RAILGUN";
-								}
+							if(BasicFunctions.ForkModus){
+								gamemode = "FORK";
+							}
+							else{
+								gamemode = "RAILGUN";
+							}
 
-								string url = "http://drproject.twi.tudelft.nl:8082/GameRegister?Server="+BasicFunctions.activeAccount.Name+"&Finished=0"+"Gamemode="+gamemode;
-								WWW www = new WWW(url);
-								StartCoroutine(WaitForGameLog(www));
+							string url = "http://drproject.twi.tudelft.nl:8082/GameRegister?Server="+BasicFunctions.activeAccount.Name+"&Finished=0"+"Gamemode="+gamemode;
+							WWW www = new WWW(url);
+							StartCoroutine(WaitForGameLog(www));
 
-								//BasicFunctions.activeAccounts[referee.winner-1];
+							//BasicFunctions.activeAccounts[referee.winner-1];
 
 
-								spawnScript.closeServerInGame ();
-						} else if (Network.isClient) {
-								spawnScript.closeClientInGame ();
-						} else if (BasicFunctions.playOffline) {
-								Application.LoadLevel ("Menu_New");
-						}
->>>>>>> e4b880dffa488eada20526a50fa4298ec1379891
+							spawnScript.closeServerInGame ();
+					} else if (Network.isClient) {
+							spawnScript.closeClientInGame ();
+					} else if (BasicFunctions.playOffline) {
+							Application.LoadLevel ("Menu_New");
+					}
 				}
 			}
 		}
