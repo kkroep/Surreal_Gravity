7d809b45b89871a1b1e29e8c2014469f0708ba04
coenbers <C.W.J.Berssenbrugge@student.tudelft.nl>
2015-01-14 19:44:26 +0100

    Kill by time script toegevoegd
    Voor smoke effect

---
+++ Assets/_Scripts/Killbytime.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Killbytime : MonoBehaviour {
+
+	public float Time2Live = 0.5f;
+	private float Time2Live_left;
+
+	void Start () {
+
+		Time2Live_left = Time2Live;
+	}
+
+	void Update () {
+		Time2Live_left -= Time.fixedDeltaTime;
+		//Debug.Log (Time2Live_left);
+		if(Time2Live_left<0)
+			Destroy (gameObject);
+	}
+}
