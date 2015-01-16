43a82a53e598eb9db08e4c53253117cb65c655ec
StevenH <stevenhelm@live.nl>
2015-01-16 17:20:20 +0100

    random level generator changes

--- Assets/_Scripts/MaterialSelect.cs
+++ Assets/_Scripts/MaterialSelect.cs
@@ -7,5 +7,19 @@ public class MaterialSelect : MonoBehaviour {
 	// Randomly select a material from the list when instantiated
 	void Start () {
 		renderer.material = MaterialList [(int) (Random.Range (0, MaterialList.Length-0.0001f))];
+		Debug.Log (this.name);
+		if(this.name.Equals("BuildingBlock1(Clone)")){
+			renderer.material.color = Color.red;
+			Debug.Log ("BuildingBlock1");
+		}
+		else if(this.name.Equals ("BuildingBlock2(Clone)")){
+			renderer.material.color = Color.cyan;
+			Debug.Log ("BuildingBlock2");
+		}
+		else if(this.name.Equals ("BuildingBlock3(Clone)")){
+			renderer.material.color = Color.green;
+			Debug.Log ("BuildingBlock3");
+		}
+
 	}
 }
