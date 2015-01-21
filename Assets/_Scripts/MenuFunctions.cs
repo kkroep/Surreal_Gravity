03ac2bfa52156398d5777afb6888b63a44748f80
Earless <roberto_mol@hotmail.com>
2015-01-21 16:40:00 +0100

    Menu button 2D

--- Assets/_Scripts/MenuFunctions.cs
+++ Assets/_Scripts/MenuFunctions.cs
@@ -112,7 +112,7 @@ public class MenuFunctions : MonoBehaviour {
 
 	public void goToAccount ()
 	{
-		AudioSource.PlayClipAtPoint(menu_click_sound, transform.position, 50f);
+		AudioSource.PlayClipAtPoint(menu_click_sound, transform.position, 0.5f);
 		Main_Menu.SetActive(false);
 		Account_Menu.SetActive(true);
 		if (BasicFunctions.activeAccount != null)
